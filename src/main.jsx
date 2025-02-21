5b7c4fd987541bf2904ead8d55ae9913b7df0b4d
Hi I'm Ujef <125216020+Ujef55@users.noreply.github.com>
2025-02-21 20:35:05 +0530

    Update main.jsx
    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
 
 
-createRoot(document.getEle('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <Toaster />
